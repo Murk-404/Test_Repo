a6c8e04d465a6bbaf956474abd81c9defefba39d
Murk-404 <105297858+Murk-404@users.noreply.github.com>
2022-08-15 22:10:11 -0500

    Add files via upload

--- website-files/public/input.js
+++ website-files/public/input.js
@@ -13,7 +13,32 @@ function getInputValue1(){
     // var xhttp = new XMLHttpRequest();
     // xhttp.open("GET", url, true);
     // xhttp.send();
-    var link1 = 'http://localhost:4000/user/top-data.html';
+
+    // var link1 = 'http://localhost:4000/user/top-data.html';
+    var link1 = 'http://localhost:4000/congrats.html';
+
+    window.location.href = link1;
+    // // print("lmao");
+}
+function getInputValue2(){
+    var url = 'http://localhost:4000/user/iuawhdoiuhseoijfhneofihSEOFuh'
+    var xhr = new XMLHttpRequest();
+    // xhr.onreadystatechange = function() {
+    //     if (xhr.readyState == XMLHttpRequest.DONE) {
+    //         console.log("IT HAPPENED");
+    //         // var link1 = 'http://localhost:4000/user/top-data';
+    //         // window.location.href = link1;
+    //     }
+    // }
+    xhr.open('GET', url, true);
+    xhr.send();
+    // var xhttp = new XMLHttpRequest();
+    // xhttp.open("GET", url, true);
+    // xhttp.send();
+
+    // var link1 = 'http://localhost:4000/user/top-data.html';
+    var link1 = 'http://localhost:4000/about.html';
+
     window.location.href = link1;
     // // print("lmao");
 }
